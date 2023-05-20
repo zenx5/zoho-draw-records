b17e2de816ab77c8fc94b25b5ddb61f11b682860
Octavio Martinez <omartinez1618@gmail.com>
2023-05-20 19:03:09 -0400

    fix(BaseService.js): add missing parentheses to getResource method call in fetch URL

--- src/services/BaseService.js
+++ src/services/BaseService.js
@@ -7,7 +7,7 @@ export default class BaseService {
 
 
     static async get({path}) {
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${this.getResource}/${path}`,{
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/${this.getResource()}/${path}`,{
             headers:{
                 user_id:1
             }
