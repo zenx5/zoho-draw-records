f4804c75861a02c5a746478d54208402bcda22b8
Octavio Martinez <omartinez1618@gmail.com>
2023-05-20 10:30:21 -0400

    render components

--- src/views/Drawing/Drawing.js
+++ src/views/Drawing/Drawing.js
@@ -1,8 +1,11 @@
 import { Box, Typography } from "@mui/material";
+import { useDrawing } from "./Context";
 
 export default function Drawing() {
+    const {current, modules} = useDrawing()
 
     return <Box>
         <Typography>Drawing Component</Typography>
+        { current && <Typography>Ha seleccionado el modulo: {modules.find( module => module.id == current)?.module_name }</Typography>}
     </Box>
 }
--- src/views/Drawing/SideBar.js
+++ src/views/Drawing/SideBar.js
@@ -1,8 +1,19 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, MenuItem, Stack, Typography } from "@mui/material";
+import { useDrawing } from "./Context";
 
 export default function SideBar() {
+    const {modules, selectIt} = useDrawing()
+
+    const handlerClick = (id) => {
+        selectIt(id)
+    }
 
     return <Box>
         <Typography>Side bar</Typography>
+        <Stack spacing={1}>
+            {
+                modules.map( module => <MenuItem key={module.id} onClick={()=>handlerClick(module.id)}>{module.module_name}</MenuItem>)
+            }
+        </Stack>
     </Box>
 }
