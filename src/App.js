2e5f4c47bded88a37a657717623c8d6e3219ffbb
Octavio Martinez <omartinez1618@gmail.com>
2023-05-20 19:04:44 -0400

    clean proyect

--- src/App.js
+++ src/App.js
@@ -3,27 +3,26 @@ import {  BrowserRouter as Router, Route, Routes, useNavigate } from 'react-rout
 import DrawingView from './views/Drawing/Index';
 
 export default function App(){
-    const navigate = useNavigate();
-    const params = new Proxy(new URLSearchParams(window.location.search), {
-        get: (searchParams, prop) => searchParams.get(prop),
-    })
-    useEffect(() => {
-      console.log(params)
-        const path = params.path
-        switch (path) {
-          case 'drawing':
-            navigate('/drawing')
-            break
-          default:
-            navigate('/app')
-            break
-        }
-    }, [])
+    // const navigate = useNavigate();
+    // const params = new Proxy(new URLSearchParams(window.location.search), {
+    //     get: (searchParams, prop) => searchParams.get(prop),
+    // })
+    // useEffect(() => {
+    //   console.log(params)
+    //     const path = params.path
+    //     switch (path) {
+    //       case 'drawing':
+    //         navigate('/drawing')
+    //         break
+    //       default:
+    //         navigate('/app')
+    //         break
+    //     }
+    // }, [])
 
     return(
         <Routes>
-            <Route path='/app' element='creado con React'/>
-            <Route path='/drawing' element={<DrawingView />}/>
+            <Route path='/app' element={<DrawingView />}/>
 
         </Routes>
     )
